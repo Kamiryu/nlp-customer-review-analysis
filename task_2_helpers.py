c4993355ab199f47d513b75ea7aa6f41eac48af1
Nihal Ezgi YUCETURK <Ezgi.YUCETURK@gunvorgroup.com>
2024-05-17 17:28:10 +0200

    tokenizer and tfodf tranform helper functions are added

--- task_2_helpers.py
+++ task_2_helpers.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import ast as ast
 import numpy as np
+import string
+# string.punctuation
+
 from typing import *
 
 
@@ -27,3 +30,55 @@ def extract_attributes_value(attr_dictionary: Dict) -> List:
         if v:
             values.append(k)
     return values
+
+
+########################################################################################################################################################
+import nltk
+# from nltk.stem import PorterStemmer
+# from nltk.stem.snowball import SnowballStemmer
+
+stopwords = nltk.corpus.stopwords.words("english")
+wnl = nltk.stem.WordNetLemmatizer()
+
+
+def custom_tokenizer(text: str, pos_arg={"VERB":'v', "ADJ":'a', "ADV":'r', "NOUN":'n'}) -> List:
+    """
+    Accepts a text as a string, tokenize and lemmatize it
+    """
+    output = []
+    text_tokens = nltk.word_tokenize(text)
+    
+    for w, pos in nltk.pos_tag(text_tokens, tagset="universal"):
+        pos = pos_arg[pos] if pos in pos_arg.keys() else 'n'
+        lem = wnl.lemmatize(w, pos=pos)
+
+        if lem not in stopwords:
+            output.append(lem)
+
+    # print the original tokens and their lemma
+    # print(f'{w} -> {lem}')
+  
+    return output
+
+
+########################################################################################################################################################
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+
+def count_word_vector(df : pd.DataFrame, column_name: str, lowercase=True) -> List[List]:
+    """
+    Returns bag-of-words representation of a given column in a dataframe.
+    It uses the custom tokenizer defined above. 
+    """
+    vectorizer = CountVectorizer(tokenizer=custom_tokenizer ,lowercase=lowercase) # min_df=1e-3 to ignore unfrequent words
+    bow_repr = vectorizer.fit_transform(df['column_name'])
+    
+    return bow_repr
+
+
+def tfidf_representation(bow_representation: List[List]) -> List[List]:
+    tfidf_transformer = TfidfTransformer()
+    tfidf_repr = tfidf_transformer.fit_transform(bow_representation)
+
+    return tfidf_repr
